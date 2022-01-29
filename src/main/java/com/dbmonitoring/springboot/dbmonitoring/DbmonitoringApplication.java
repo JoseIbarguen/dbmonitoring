a6547d74994f121f587122c0e15a037f8a6a7677
JoDa96 <joseibarguenp@gmail.com>
2022-01-28 20:53:34 -0500

    First commit & Setup

---
+++ src/main/java/com/dbmonitoring/springboot/dbmonitoring/DbmonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.dbmonitoring.springboot.dbmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbmonitoring/springboot/dbmonitoring/DbmonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbmonitoring.springboot.dbmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
